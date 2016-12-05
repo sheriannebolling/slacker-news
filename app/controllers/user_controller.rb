6cea3093470793413b50a60e7c59c9d06106cc86
Sherianne Bolling <sheriannebolling@gmail.com>
2016-12-05 11:41:12 -0500

    Set up basic structure

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,17 @@
 class UserController < ActionController::Base
   def new
-      @user = User.new 
+    @user = User.new 
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "You signed up successfully"
+      flash[:color]= "valid"
+    else
+      flash[:notice] = "Form is invalid"
+      flash[:color]= "invalid"
     end
-    def create
-      @user = User.new(params[:user])
-      if @user.save
-        flash[:notice] = "You signed up successfully"
-        flash[:color]= "valid"
-      else
-        flash[:notice] = "Form is invalid"
-        flash[:color]= "invalid"
-      end
-      render "new"
+    render "new"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,11 @@ class User < ApplicationRecord
     attr_accessor :password
   EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
-  validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
+  validates :email, :presence => true, :uniqueness => true
   validates :password, :confirmation => true #password_confirmation attr
   validates_length_of :password, :in => 6..20, :on => :create
+  
+  has_many :posts
+  has_many :comments
+
 end
